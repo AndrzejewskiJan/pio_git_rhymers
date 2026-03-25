7338e670613a30c5de8203893a86504921ff7bd8
Jędrzej Pokrywiecki <250992@edu.p.lodz.pl>
2026-03-25 21:14:54 +0100

    Dodanie stałych, final i @Override

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,10 +1,13 @@
 package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
+	
+	public static final int SIZE = 12;
+    public static final int TOTAL = -1;
+	
+    private int[] NUMBERS = new int[SIZE];
 
-    private int[] NUMBERS = new int[12];
-
-    public int total = -1;
+    public int total = TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,7 +15,7 @@ public class defaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL;
     }
 
     public boolean isFull() {
@@ -21,13 +24,13 @@ public class defaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return TOTAL;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return TOTAL;
         return NUMBERS[total--];
     }
 
